bcad5a2d938905bc1797dd00d0edec12d747fc63
pinocchio22 <vviian.2@gmail.com>
2022-09-14 22:50:21 +0900

    DecoupledConstraintLayout

--- app/src/main/java/com/example/codelab_day_02/MainActivity.kt
+++ app/src/main/java/com/example/codelab_day_02/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,17 +18,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import coil.compose.rememberImagePainter
 import com.example.codelab_day_02.ui.theme.Codelab_day_02Theme
 import com.example.codelab_day_02.ui.theme.Teal200
@@ -54,9 +56,95 @@ class MainActivity : ComponentActivity() {
 //                LayoutsCodelab()
 //                MyColumnTest()
 //                MyRowTest()
-                StaggeredGridExample()
+//                StaggeredGridExample()
+//                ConstraintLayoutContent()
+                DecoupledConstraintLayout()
             }
         }
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+
+    val buttonId: String = "MyBtn"
+    val addingMargindBtnID: String = "addingMargindBtnID"
+    val textId: String = "MyText"
+
+    // 추가된 margin
+    val addedMargin = remember{ mutableStateOf(0.dp) }
+
+    val animatedMargin = animateDpAsState(targetValue = addedMargin.value)
+
+    BoxWithConstraints {
+        val constraints = if (minWidth < 600.dp) {
+            decoupledConstraints(addingMargindBtnID, buttonId, textId, margin = 16.dp, addedMargin = animatedMargin.value)// Portrait constraints
+        } else {
+            decoupledConstraints(addingMargindBtnID, buttonId, textId, margin = 100.dp, addedMargin = animatedMargin.value) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId(buttonId)
+            ) {
+                Text("Button")
+            }
+
+            Button(
+                onClick = {
+                    addedMargin.value = addedMargin.value + 10.dp
+                },
+            modifier = Modifier.layoutId(addingMargindBtnID)) {
+                Text(text = "마진 추가 버튼")
+            }
+
+            Text("Text", Modifier.layoutId(textId))
+        }
+    }
+}
+
+private fun decoupledConstraints(addingMargindBtnID: String, buttonId: String, textId: String, margin: Dp, addedMargin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor(buttonId)
+        val text = createRefFor(textId)
+        val addingMargindBtn = createRefFor(addingMargindBtnID)
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+        constrain(addingMargindBtn) {
+            top.linkTo(text.bottom, addedMargin)
+        }
+    }
+}
+
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        // Create references for the composables to constrain
+        val (buttonRef, textRef) = createRefs()
+
+        Button(
+            onClick = { /* Do something */ },
+            // Assign reference "button" to the Button composable
+            // and constrain it to the top of the ConstraintLayout
+            modifier = Modifier.constrainAs(buttonRef) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button")
+        }
+
+        // Assign reference "text" to the Text composable
+        // and constrain it to the bottom of the Button composable
+        Text("Text", Modifier.constrainAs(textRef) {
+            top.linkTo(buttonRef.bottom, margin = 16.dp)
+            centerHorizontallyTo(buttonRef)
+        })
     }
 }
 
@@ -99,9 +187,18 @@ fun MyColumnTest() {
 fun MyRowTest() {
     Scaffold() {
         MyOwnRow(modifier = Modifier.background(Color.Yellow)) {
-            Text(text = "aaaaaaaaaaaaaaa", modifier = Modifier.padding(4.dp).background(Color.Green).height(100.dp))
-            Text(text = "aaaaa", modifier = Modifier.padding(4.dp).background(Color.Magenta).height(30.dp))
-            Text(text = "aaaaaaaaaaa", modifier = Modifier.padding(4.dp).background(Color.Blue).height(50.dp))
+            Text(text = "aaaaaaaaaaaaaaa", modifier = Modifier
+                .padding(4.dp)
+                .background(Color.Green)
+                .height(100.dp))
+            Text(text = "aaaaa", modifier = Modifier
+                .padding(4.dp)
+                .background(Color.Magenta)
+                .height(30.dp))
+            Text(text = "aaaaaaaaaaa", modifier = Modifier
+                .padding(4.dp)
+                .background(Color.Blue)
+                .height(50.dp))
         }
     }
 }
